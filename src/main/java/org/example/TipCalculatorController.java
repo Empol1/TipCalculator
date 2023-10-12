2e07268c129d45b36ee46333907b8e9379f99e0a
Meraj <vipermma253@gmail.com>
2023-10-11 22:30:44 -0400

    Initial commit

---
+++ src/main/java/org/example/TipCalculatorController.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class TipCalculator {
+
+    @FXML
+    private Label topLabel;
+    @FXML
+    private void clickOnokButton() { topLabel.setText("Twerk");
+
+
+    }
+}
